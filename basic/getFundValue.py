dbfb727b4f727d801e146aba7d214469d3243b89
laizhengyang <laizhengyang@cmdi.chinamobile.com>
2024-12-18 17:31:17 +0800

    基金指标计算

---
+++ basic/getFundValue.py
@@ -0,0 +1,7 @@
+import akshare as ak
+
+fund_open_fund_info_em_df = ak.fund_open_fund_info_em(symbol="710001", indicator="累计净值走势")
+print(fund_open_fund_info_em_df)
+
+fund_open_fund_info_em_df = ak.fund_open_fund_info_em(symbol="710001", indicator="单位净值走势")
+print(fund_open_fund_info_em_df)
---
+++ test2.py
@@ -0,0 +1,4 @@
+import akshare as ak
+
+fund_individual_analysis_xq_df = ak.fund_individual_analysis_xq(symbol="004832")
+print(fund_individual_analysis_xq_df)
