ea1c5288dfda6c067637d15424f8000e5d82dce5
laizhengyang <laizhengyang@cmdi.chinamobile.com>
2025-02-27 17:25:03 +0800

    add:system

--- indicator/daily/ATR.py
+++ indicator/daily/ATR.py
@@ -24,7 +24,7 @@ def calculate_atr(fund_code: str, period: int = 14):
         ORDER BY net_value_date ASC;
         """
         df = pd.read_sql(query, connection, params=(fund_code,))
-
+        df["net_value_date"] = pd.to_datetime(df["net_value_date"])  # 确保日期是日期格式
         # 计算真实波幅（True Range）
         df['previous_close'] = df['close_value'].shift(1)
         df['true_range'] = df[['high_value', 'low_value', 'previous_close']].apply(
@@ -33,11 +33,12 @@ def calculate_atr(fund_code: str, period: int = 14):
                             abs(row['low_value'] - row['previous_close'])), axis=1
         )
 
-        # 计算ATR（14日平均真实波幅）
-        df['atr'] = df['true_range'].rolling(window=period).mean()
+        # 动态计算ATR（根据周期）
+        atr_column_name = f'ATR{period}'  # 动态列名
+        df[atr_column_name] = df['true_range'].rolling(window=period).mean()
 
         # 获取结果并返回
-        result = df[['fund_code', 'net_value_date', 'close_value', 'atr']].dropna()
+        result = df[['fund_code', 'net_value_date', 'open_value', 'close_value', 'high_value', 'low_value', 'true_range', atr_column_name]].dropna()
 
         return result
 
@@ -47,6 +48,8 @@ def calculate_atr(fund_code: str, period: int = 14):
 
 
 # 示例调用
-fund_code = '510300'
-atr_result = calculate_atr(fund_code)
-print(atr_result)
+# fund_code = '510300'
+# period = 20  # 可以修改为其他周期，例如30, 60等
+# atr_result = calculate_atr(fund_code, period)
+# atr_result.to_excel(f'../data/{fund_code}_ATR_{period}.xlsx', index=False)
+# print(atr_result)
--- indicator/daily/MA.py
+++ indicator/daily/MA.py
@@ -21,41 +21,33 @@ def calculate_ma(fund_code, ma_days=40):
     :return: 包含移动平均和累计净值的DataFrame
     """
     # 连接MySQL数据库
-    conn = pymysql.connect(**db_config)
-    cursor = conn.cursor()
+    connection = pymysql.connect(**db_config)
+
 
     try:
         # 查询特定基金的净值数据，按日期排序
         query = """
-        SELECT fund_code, net_value_date, cumulative_net_value
-        FROM etf_net_value
+        SELECT fund_code, net_value_date, close_value
+        FROM etf_net_value_qfq
         WHERE fund_code = %s
         ORDER BY net_value_date;
         """
-        cursor.execute(query, (fund_code,))
-        data = cursor.fetchall()
-
-        # 如果没有数据，直接返回空的DataFrame
-        if not data:
-            return pd.DataFrame()
-
-        # 将查询结果转换为DataFrame
-        df = pd.DataFrame(data, columns=["fund_code", "net_value_date", "cumulative_net_value"])
+        df = pd.read_sql(query, connection, params=(fund_code,))
         df["net_value_date"] = pd.to_datetime(df["net_value_date"])  # 确保日期是日期格式
 
         # 计算指定天数的移动平均
         ma_column = f"ma{ma_days}"
-        df[ma_column] = df["cumulative_net_value"].rolling(window=ma_days, min_periods=1).mean()
+        df[ma_column] = df["close_value"].rolling(window=ma_days, min_periods=1).mean()
 
         # 返回结果，包括基金代码、日期、当日净值以及指定天数的MA列
-        return df[["fund_code", "net_value_date", "cumulative_net_value", ma_column]]
+        result = df[["fund_code", "net_value_date", "close_value", ma_column]]
+        return result
 
     except Exception as e:
         print("查询或计算出现错误:", e)
         return pd.DataFrame()
     finally:
-        cursor.close()
-        conn.close()
+        connection.close()
 
 
 
@@ -65,17 +57,19 @@ def calculate_ma(fund_code, ma_days=40):
 :param fund_code: 基金代码
 :return: 包含40日和100日均线的DataFrame
 """
-# 获取40日均线
-result_40 = calculate_ma('510300', ma_days=40)
 
-# 获取100日均线
-result_100 = calculate_ma('510300', ma_days=100)
-
-# 合并40日和100日均线的结果
-combined_result = pd.merge(result_40, result_100, on=["fund_code", "net_value_date", "cumulative_net_value"],
-                           how="outer")
-combined_result.to_excel('../data/510300.xlsx', index=False)
-# 打印合并后的结果
-print(combined_result)
+# fund_code = '510300'
+# # 获取40日均线
+# result_40 = calculate_ma(fund_code, ma_days=40)
+#
+# # 获取100日均线
+# result_100 = calculate_ma(fund_code, ma_days=100)
+#
+# # 合并40日和100日均线的结果
+# combined_result = pd.merge(result_40, result_100, on=["fund_code", "net_value_date", "close_value"],
+#                            how="outer")
+# combined_result.to_excel('../data/510300_ma.xlsx', index=False)
+# # 打印合并后的结果
+# print(combined_result)
 
 
